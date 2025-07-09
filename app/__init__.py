d1788251faad2a12868d0cfc8f19bf8d8a65a7d8
alexandre <alexandremos@gmail.com>
2025-07-09 08:46:42 -0300

    mensagem sms

--- app/__init__.py
+++ app/__init__.py
@@ -3,37 +3,41 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from app.db import criar_tabela  # função sqlite3 pura para tabela OS
+from flask_mail import Mail
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'main.login'
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ordem_servico'
 
-    # Caminho absoluto fixo para a pasta banco
+    # Caminho para banco
     base_dir = os.path.abspath(os.path.dirname(__file__))
-
-   # Caminho absoluto fixo para a pasta banco
-   # banco_dir = r"C:\ForPoint\V3\ordem_servico_app\banco"
-
     banco_dir = os.path.join(base_dir, '..', 'banco')
-    os.makedirs(banco_dir, exist_ok=True)  # cria a pasta banco se não existir
-
+    os.makedirs(banco_dir, exist_ok=True)
     db_path = os.path.join(banco_dir, 'solicitacoes.db')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-   
+    # Configurações do e-mail
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'alexandremos@gmail.com'
+    app.config['MAIL_PASSWORD'] = os.getenv('GMAIL_APP_PASSWORD')
+ 
+    # Inicializa extensões
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     with app.app_context():
-        from app.models import User  # ← ADICIONE ESTA LINHA
-        criar_tabela()  # cria tabela das OS no banco sqlite3 (se não existir)
-        db.create_all()  # cria tabela dos usuários com SQLAlchemy (se não existir)
+        from app.models import User  # importa modelo User
+        criar_tabela()              # cria tabela das OS (sqlite3 pura)
+        db.create_all()             # cria tabelas SQLAlchemy (ex: User)
 
     from app.routes import main
     app.register_blueprint(main)
@@ -44,4 +48,3 @@ def create_app():
 def load_user(user_id):
     from app.models import User
     return User.query.get(int(user_id))
-
